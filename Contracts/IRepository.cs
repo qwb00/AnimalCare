f40250c3d416d7df1604ab3b0a3d6da61bfa6f3c
usennon <mikhailvorobevandr@gmail.com>
2024-10-19 17:26:20 +0300

    Add: service anmial layer

--- Contracts/IRepository.cs
+++ Contracts/IRepository.cs
@@ -5,6 +5,7 @@ namespace Contracts
 {
     public interface IRepository<T> where T : BaseEntity, new()
     {
+        Task<T> GetByIdAsync(Guid id, bool trackChanges);
         void CreateRange(IEnumerable<T> entities);
         void Update(T entity);
         void Delete(T entity);
--- Repositories/RepositoryBase.cs
+++ Repositories/RepositoryBase.cs
@@ -26,6 +26,9 @@ namespace Repositories
         protected IQueryable<T> GetAll(bool trackChanges) => !trackChanges ?
             _dbSet.AsNoTracking() : _dbSet;
 
+        public async Task<T> GetByIdAsync(Guid id, bool trackChanges)
+            => await GetByCondition(e => e.Id == id, trackChanges).SingleOrDefaultAsync();
+
         protected void Create(T entity) => _dbSet.Add(entity);
 
         public virtual void CreateRange(IEnumerable<T> entities) => _dbSet.AddRange(entities);
