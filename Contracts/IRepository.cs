e4380dba29b069b669ff47a7ca6e52788dcad454
qwb00 <sasha.postelga@gmail.com>
2024-10-19 19:51:02 +0200

    push b

--- Contracts/IRepository.cs
+++ Contracts/IRepository.cs
@@ -5,7 +5,7 @@ namespace Contracts
 {
     public interface IRepository<T> where T : BaseEntity, new()
     {
-        Task<T> GetByIdAsync(Guid id, bool trackChanges);
+        Task<T> GetByIdAsync(Guid id, bool trackChanges, params Expression<Func<T, object>>[] includes);
         void CreateRange(IEnumerable<T> entities);
         void Update(T entity);
         void Delete(T entity);
