537ac393d30ef95667deb77d59a050b7c78d2a74
usennon <mikhailvorobevandr@gmail.com>
2024-10-26 18:34:07 +0300

    Login added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Animals from './pages/Animals';
 import Reservations from './pages/Reservations';
+import LoginPage from './pages/LoginPage';
 import BackToTop from './components/BackToTop'; // Импортируем якорь
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/animals" element={<Animals />} />
         <Route path="/reservations" element={<Reservations />} />
+        <Route path="/login" element={<LoginPage />} /> 
+       
       </Routes>
       <BackToTop /> {/* Добавляем якорь */}
     </Router>
