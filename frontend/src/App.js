613cb38d6574d427e29c72bea8fac07d500bcc59
BananaMonsterlele <alex.2001.ru@gmail.com>
2024-10-27 14:48:47 +0100

    test commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
-import {  useNavigate, Route, Routes } from 'react-router-dom';
+import { useNavigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Animals from './pages/Animals';
 import Reservations from './pages/Reservations';
 import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage'
-import BackToTop from './components/BackToTop'; // Импортируем якорь
+import RegisterPage from './pages/RegisterPage';
+import BackToTop from './components/BackToTop';
 import AnimalDetails from './pages/AnimalDetails';
 
 function App() {
@@ -20,17 +20,18 @@ function App() {
       navigate('/login');
     }
   }, [navigate]);
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/animals" element={<Animals />} />
         <Route path="/reservations" element={<Reservations />} />
-        <Route path="/animals/:id" element={<AnimalDetails />} /> {/* Используем element вместо component */}
+        <Route path="/animals/:id" element={<AnimalDetails />} />
         <Route path="/login" element={<LoginPage />} /> 
         <Route path="/signup" element={<RegisterPage />} />
       </Routes>
-      <BackToTop /> {/* Добавляем якорь */}
+      <BackToTop />
     </>
   );
 }
