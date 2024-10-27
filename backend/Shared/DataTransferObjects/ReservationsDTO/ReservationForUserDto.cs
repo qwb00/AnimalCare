8d9511ea910188cd3c93e1c8e998ed3f56245f3b
qwb00 <sasha.postelga@gmail.com>
2024-10-27 22:46:35 +0100

    Fixed problems with getting reservations

--- backend/Shared/DataTransferObjects/ReservationsDTO/ReservationForUserDto.cs
+++ backend/Shared/DataTransferObjects/ReservationsDTO/ReservationForUserDto.cs
@@ -8,5 +8,9 @@ namespace Shared.DataTransferObjects.ReservationsDTO
         public string AnimalName { get; set; }
         public string AnimalBreed { get; set; }
         public ReservationStatus Status { get; set; }
+        
+        public DateTime Date { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
     }
 }
