e4bccb082be63cbb2369820abfcfde9a74995286
usennon <mikhailvorobevandr@gmail.com>
2024-10-28 03:01:23 +0300

    Added treatments page prototype

--- frontend/src/components/UserNav.js
+++ frontend/src/components/UserNav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function UserNav({ role }) {
     // Default to an empty role if undefined
@@ -18,9 +19,9 @@ function UserNav({ role }) {
     return (
         <div className="flex space-x-8 mt-6 mb-8 ml-16 md:ml-24 xl:ml-36 text-gray-500 text-lg">
             {itemsToShow.includes("General") && (
-                <span className="text-blue-500 font-semibold border-b-2 border-blue-500 cursor-pointer">
+                 <Link to="/:userID/veterinarian-examinations" className="text-blue-500 font-semibold border-b-2 border-blue-500 cursor-pointer">
                     General
-                </span>
+                </Link>
             )}
             {itemsToShow.includes("Volunteers") && (
                 <span className="cursor-pointer hover:text-blue-500">Volunteers</span>
@@ -29,7 +30,9 @@ function UserNav({ role }) {
                 <span className="cursor-pointer hover:text-blue-500">Reservations</span>
             )}
             {itemsToShow.includes("Medical Treatments") && (
-                <span className="cursor-pointer hover:text-blue-500">Medical Treatments</span>
+                <Link to="/:userID/veterinarian-examinations" className="cursor-pointer hover:text-blue-500">
+                    Medical Treatments
+                </Link>
             )}
         </div>
     );
