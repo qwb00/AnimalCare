ceacd8547111caefe2af6ccb3d5bc159d70c1768
usennon <mikhailvorobevandr@gmail.com>
2024-11-24 21:05:34 +0100

    fixed patch user

--- backend/Shared/DataTransferObjects/UsersDTO/UserForUpdateDTO.cs
+++ backend/Shared/DataTransferObjects/UsersDTO/UserForUpdateDTO.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Shared.DataTransferObjects.UsersDTO
 {
-    public record UserForUpdateDTO : UserForManipulationDTO
+    public record UserForUpdateDTO
     {
+        [MaxLength(50, ErrorMessage = "Name is too long")]
+        [RegularExpression(@"^[a-zA-Z\s\-]+$", ErrorMessage = "Name can only contain English letters, spaces, and hyphens.")]
+        public string? FirstName { get; init; }
+
+        [MaxLength(50, ErrorMessage = "Name is too long")]
+        [RegularExpression(@"^[a-zA-Z\s\-]+$", ErrorMessage = "Name can only contain English letters, spaces, and hyphens.")]
+        public string? LastName { get; init; }
+
+        [EmailAddress(ErrorMessage = "Invalid email address format")]
+        public string? Email { get; init; }
+
+        [RegularExpression(@"^\+?\d{10,15}$", ErrorMessage = "Invalid phone number format. Must be from 10 to 15 digits")]
+        public string? PhoneNumber { get; init; }
         public bool? isActive { get; set; }
     }
 }
