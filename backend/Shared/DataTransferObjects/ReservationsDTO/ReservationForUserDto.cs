4496e0fabe4ad17e9747305bba4cfdadff4971a6
qwb00 <sasha.postelga@gmail.com>
2024-12-08 22:53:58 +0100

    Added animalid to reservations for user

--- backend/Shared/DataTransferObjects/ReservationsDTO/ReservationForUserDto.cs
+++ backend/Shared/DataTransferObjects/ReservationsDTO/ReservationForUserDto.cs
@@ -5,6 +5,7 @@ namespace Shared.DataTransferObjects.ReservationsDTO
     public class ReservationForUserDTO
     {
         public Guid Id { get; set; }
+        public Guid AnimalId { get; set; }
         public string AnimalName { get; set; }
         public string AnimalBreed { get; set; }
         public ReservationStatus Status { get; set; }
